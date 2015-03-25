fee21eae642e3f372a38c4b6b342a222a9f5f3fb
Virgil Roger <roger.virgil@gmail.com>
2015-03-25 17:42:14 +0100

    Posts load tests
    get posts by category

--- js/controllers/main-controller.js
+++ js/controllers/main-controller.js
@@ -3,9 +3,17 @@
 angular.module('girphoto')
 	.controller('mainController', ['$scope', '$http', function($scope, $http) {
 
-		$http.get('http://localhost/gir_rework/wp-json/posts?filter[category_name]=TEST&filter[order]=ASC').then(function(response) {
-			$scope.photos = response.data;
-			console.log($scope.photos);
-		});
+		$scope.loadPhotos = function(cat_id) {
+			$http.get('http://localhost/gir_rework/wp-json/posts?filter[category_name]='+cat_id+'&filter[order]=ASC').then(function(response) {
+				$scope.photos = response.data;
+				console.log($scope.photos);
+			});
+		};
+
+		$scope.navVisible = false;
+
+		$scope.toggleNav = function() {
+			$scope.navVisible = !$scope.navVisible;
+		};
 
 	}]);
